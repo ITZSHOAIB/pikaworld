eca12a157deb761476ceb4eb40a8e49ea7d7d4f9
Sohab <skshoaibpersonal@gmail.com>
2022-02-16 03:58:31 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import Nav from "./components/Nav";
 import Card from "./components/Card";
 import PokemonContextProvider from "./context/pokemon-context";
 
-import "./App.css";
-
 function App() {
   return (
     <main className="font-mono w-screen h-screen bg-gradient-to-b from-pink-500 via-red-500 to-yellow-500 sm:p-0 overflow-auto ">
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -23,7 +23,7 @@ function Nav() {
     <>
       <nav className="flex justify-center flex-wrap sm:flex-nowrap content-between sm:justify-between items-center px-5 sm:py-0  sm:shadow-sm sm:h-10 bg-transparent sm:bg-gradient-to-r sm:from-white sm:via-transparent sm:to-transparent">
         <div className="font-bold text-2xl rounded-full mr-2 text-sky-900 hover:bg-white px-4 sm:px-0 py-2 sm:py-0 hover:text-red-500 hover:mb-2 sm:hover:mb-0 hover:cursor-pointer flex justify-center items-center">
-          <img src={logo} alt="" className="block w-5 mr-1" />{" "}
+          <img src={logo} alt="" className="block w-8 mr-1" />{" "}
           <h1>PikaSearch</h1>
         </div>
         <input
