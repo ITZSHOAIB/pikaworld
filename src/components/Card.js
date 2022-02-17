8fffe1e414bd9e726d13439947bfd135d13a101d
Sohab <skshoaibpersonal@gmail.com>
2022-02-17 14:02:12 +0530

    card changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -18,41 +18,56 @@ function Card(props) {
 
   const { name, stats, weight } = pokemon.data;
 
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      fetchPokemon(event.target.value.toLowerCase())
+        .then((data) => {
+          setPokemon(data);
+        })
+        .catch(() => console.log("No Pokemon Found with this name..."))
+        .finally(() => {
+          event.target.value = "";
+        });
+    }
+  };
   return (
     <>
-      <div className="rounded-md text-center p-4 shadow-sm mt-5 mx-auto max-w-md bg-black/40 backdrop-blur-xl">
+      <div className="rounded-md text-center p-4 shadow-sm mt-5 mx-auto max-w-md bg-black/20 backdrop-blur-xl">
+        <input
+          type="text"
+          className="rounded-full placeholder:text-center placeholder:italic focus:outline-none px-4 py-2 focus:placeholder:text-white/60 w-full max-w-md h-8 text-white/60 border-b-2 border-white/80 bg-transparent mb-5"
+          placeholder="Enter your Pokemon name"
+          onKeyPress={handleKeyPress}
+        />
         <div className=" text-white/90">
           <div className="">
             <img
               src={pokemon.sprite}
               alt={`Imagen: ${name}`}
-              className="rounded-lg h-52 w-52 sm:h-72 sm:w-72 mb-8 mx-auto"
+              className="rounded-lg w-full bg-black/30 h-52 w-52 sm:h-72 sm:w-72 mb-4 mx-auto p-4"
             />
           </div>
-          <h1 className="font-semibold text-white bg-pink-500 rounded-full py-1 my-2 w-4/5 mx-auto">
+          <h1 className="font-semibold text-white bg-pink-500 py-1 my-2 w-full mx-auto">
             {" "}
             {name.toUpperCase()}{" "}
           </h1>
 
-          <section className="border border-white/60 p-2">
-            <ul className="grid grid-rows-6">
-              <h1 className="font-bold bg-black/20 text-center">
-                CHARACTERISTICS
-              </h1>
+          <section className="border-b-2 mb-3 p-2">
+            <ul className="grid grid-rows-6">              
               {stats.map((object) => {
                 return (
                   <li key={object.stat.name} className="grid grid-cols-2">
-                    <span className="font-semibold">
+                    <span className="text-left">
                       {object.stat.name.toUpperCase()}
                     </span>
-                    <span className="text-sky-300">{object.base_stat}</span>
+                    <span className="text-sky-300 font-semibold text-right">{object.base_stat}</span>
                   </li>
                 );
               })}
             </ul>
           </section>
 
-          <footer className="flex justify-end">
+          <footer className="flex justify-center">
             <span className="text-gray-200 text-sm">WEIGHT:{weight}</span>
           </footer>
         </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,37 +1,14 @@
-import { useContext } from "react";
 import logo from "../logo.svg";
 
-import { fetchPokemon } from "../services/fetchApi";
-import { PokemonContext } from "../context/pokemon-context";
-
 function Nav() {
-  const { setPokemon } = useContext(PokemonContext);
-
-  const handleKeyPress = (event) => {
-    if (event.key === "Enter") {
-      fetchPokemon(event.target.value.toLowerCase())
-        .then((data) => {
-          setPokemon(data);
-        })
-        .catch(() => console.log("No Pokemon Found with this name..."))
-        .finally(() => {
-          event.target.value = "";
-        });
-    }
-  };
+  
   return (
     <>
       <nav className="flex justify-center flex-wrap sm:flex-nowrap content-between sm:justify-between items-center px-5 sm:py-0  sm:shadow-sm sm:h-10 bg-transparent sm:bg-gradient-to-r sm:from-white sm:via-transparent sm:to-transparent">
         <div className="font-bold text-2xl rounded-full mr-2 text-sky-900 hover:bg-white px-4 sm:px-0 py-2 sm:py-0 hover:text-red-500 hover:mb-2 sm:hover:mb-0 hover:cursor-pointer flex justify-center items-center">
           <img src={logo} alt="" className="block w-8 mr-1" />{" "}
           <h1>PikaSearch</h1>
-        </div>
-        <input
-          type="text"
-          className="rounded-full placeholder:text-center placeholder:italic  focus:outline-none px-4 py-2 focus:ring-red-500 focus:ring-1 focus:placeholder:text-sky-900 w-full max-w-md h-8 text-sky-900 border sm:border-0 bg-white opacity-50"
-          placeholder="Enter your Pokemon name"
-          onKeyPress={handleKeyPress}
-        />
+        </div>        
       </nav>
     </>
   );
